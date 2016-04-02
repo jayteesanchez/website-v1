51731bb9f80b2fdfeaea353bf77803a632e519e4
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2016-04-02 15:11:55 -0700

    remove contact links to place them under home text

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -31,60 +31,41 @@ class Navbar extends React.Component {
     return (
        <nav className="navbar fadeIn animated">
           <div className="container">
-            <span className='navbar-header shake animated'>
-              <Link to='/' className="navbar-brand">
-                <img id='name-brand' className=' col-xs-12 shake animated-8' src='/img/nameBrand.png'/>
-              </Link>
-            </span>
-            <div className='col-xs-12 col-sm-6 col-sm-offset-1 col-md-5 col-md-offset-1'>
-              <span className='navbar-right' role="presentation">
-              <Link to='/resume' className='nav-links' id='resume'>
-                <img className='shake animated-5'  src='/img/newRes.png'/>
-              </Link>
-              </span>
-              <span className='navbar-right' role="presentation">
-                <Link to='/projects' className='nav-links' id='projects'>
-                <img className='shake animated-6' src='/img/newProj.png'/>
-                </Link>
-              </span>
-              <span className='navbar-right' role="presentation">
-                <Link to='/technologies' className='nav-links' id='technologies'>
-                <img className='shake animated-7'  src='/img/newTech.png'/>
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-2">
+              <span className='navbar-header shake animated'>
+                <Link to='/' className="navbar-brand">
+                  <img id='name-brand' className=' col-xs-12 shake animated-8' src='/img/nameBrand.png'/>
                 </Link>
               </span>
             </div>
-            <div className='col-xs-12 col-sm-3 col-md-3 col-md-offset-1'>
-              <span className='navbar-right' role="presentation">
-                <a className='icon-nav' href='mailto:jaytee.sanchez@gmail.com' id='resumeNav'>
-                <img className='shake animated-1' src='/img/emailjsnew.png'/>
-                  <span className='btnBefore'></span>
-                  <span className='btnAfter'></span>
-                </a>
-              </span>
-              <span className='navbar-right' role="presentation">
-                <a className='icon-nav' href='https://github.com/jayteesanchez' id='resumeNav'>
-                <img className='shake animated-2' src='/img/githubnew.png'/>
-                  <span className='btnBefore'></span>
-                  <span className='btnAfter'></span>
-                </a>
-              </span>
-              <span className='navbar-right' role="presentation">
-                <a className='icon-nav' href='https://www.facebook.com/jaytee.sanchez' id='resumeNav'>
-                <img className='shake animated-3' src='/img/facebookjsnew.png'/>
-                  <span className='btnBefore'></span>
-                  <span className='btnAfter'></span>
-                </a>
-              </span>
-              <span className='navbar-right' role="presentation">
-                <a className='icon-nav' href='https://www.linkedin.com/in/jayteesanchez' id='resumeNav'>
-                <img className='shake animated-4' src='/img/linkedinjnew2.png'/>
-                  <span className='btnBefore'></span>
-                  <span className='btnAfter'></span>
-                </a>
-              </span>
+            <div className='col-xs-12 col-sm-6 col-md-4 col-md-offset-2'>
+              <div className='col-xs-4 col-sm-4 col-md-4 no-margin'>
+                <span role="presentation">
+                <Link to='/resume' className='nav-links' id='resume'>
+                  <img className='shake animated-5'  src='/img/newRes.png'/>
+                </Link>
+                </span>
+              </div>
+              <div className='col-xs-4 col-sm-4 col-md-4 no-margin'>
+                <span role="presentation">
+                  <Link to='/projects' className='nav-links' id='projects'>
+                  <img className='shake animated-6' src='/img/newProj.png'/>
+                  </Link>
+                </span>
+              </div>
+              <div className='col-xs-4 col-sm-4 col-md-4 no-margin'>
+                <span role="presentation">
+                  <Link to='/technologies' className='nav-links' id='technologies'>
+                  <img className='shake animated-7'  src='/img/newTech.png'/>
+                  </Link>
+                </span>
+              </div>
             </div>
+                       
           </div>
-        </nav>
+        </div>
+      </nav>
     );
   }
 }
