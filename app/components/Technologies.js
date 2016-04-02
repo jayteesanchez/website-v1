9da02ccfbde92c9635a94fb9adac997b4891135e
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2016-04-02 15:13:08 -0700

    add style

--- app/components/Technologies.js
+++ app/components/Technologies.js
@@ -32,18 +32,25 @@ class Technology extends React.Component {
 
   render() {
     return (
-      <div className='container'>
-        <div className='col-xs-6 col-sm-8 col-md-8 col-sm-offset-2 col-md-offset-2 tech-images text-center'>
-          <text className="tech-text">My specialties are in...</text><br/>
-          <img className='flipInX animated-1 tech-image' src='http://moduscreate.com/wp-content/uploads/2014/03/react-opti.png'/>
-          <img className='flipInX animated-2 tech-image' src='https://node-os.com/images/nodejs.png'/>
-          <img className='flipInX animated-3 tech-image' src='http://jstricks.com/wp-content/uploads/2014/10/mongodb-gui-tools.png'/>
-          <img className='flipInX animated-4 tech-image' src='https://luisfarzati.github.io/angulartics/images/angularjs.png'/>
-          <img className='flipInX animated-5 tech-image' src='https://s3.amazonaws.com/codementor_content/2014-Aug-Week4-interviews/Ruby_on_Rails-logo.png'/>
-          <img className='flipInX animated-6 tech-image' src='http://www.hortongroup.com/sites/default/files/tech-icons-css-html.jpg'/>
-          <img className='flipInX animated-7 tech-image' src='http://www.pinyourclient.com/wp-content/uploads/2015/01/9455413519_javascript.png'/>
-          <img className='flipInX animated-8 tech-image' src='http://blog.falafel.com/wp-content/uploads/2015/01/JS6_Logo.png'/>
-          <img className='flipInX animated-9 tech-image' src='https://www.pubnub.com/blog/wp-content/uploads/2014/07/SOCKETIOICON.gif'/>
+      <div className='container fadeIn animated'>
+        <div className='col-xs-10 col-sm-10 col-md-8 col-md-offset-2 tech-images text-center'>
+          <div className="panel panel-default text-center">
+            <br></br>
+            <text className='project-title'>My specialties are in...</text>
+            <br></br>
+            <br></br>
+          </div>
+          <div className="panel panel-default text-center">
+            <img className='flipInX animated-1 tech-image' src='http://moduscreate.com/wp-content/uploads/2014/03/react-opti.png'/>
+            <img className='flipInX animated-2 tech-image' src='https://node-os.com/images/nodejs.png'/>
+            <img className='flipInX animated-3 tech-image' src='http://jstricks.com/wp-content/uploads/2014/10/mongodb-gui-tools.png'/>
+            <img className='flipInX animated-4 tech-image' src='https://luisfarzati.github.io/angulartics/images/angularjs.png'/>
+            <img className='flipInX animated-5 tech-image' src='https://s3.amazonaws.com/codementor_content/2014-Aug-Week4-interviews/Ruby_on_Rails-logo.png'/>
+            <img className='flipInX animated-6 tech-image' src='http://www.hortongroup.com/sites/default/files/tech-icons-css-html.jpg'/>
+            <img className='flipInX animated-7 tech-image' src='http://www.pinyourclient.com/wp-content/uploads/2015/01/9455413519_javascript.png'/>
+            <img className='flipInX animated-8 tech-image' src='http://blog.falafel.com/wp-content/uploads/2015/01/JS6_Logo.png'/>
+            <img className='flipInX animated-9 tech-image' src='https://www.pubnub.com/blog/wp-content/uploads/2014/07/SOCKETIOICON.gif'/>
+          </div>
         </div>
       </div>
     )
