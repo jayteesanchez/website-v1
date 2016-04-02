dbb1359ed1555a8d6860399c421160d241258f74
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2016-04-02 15:12:49 -0700

    style add to under construction text

--- app/components/Resume.js
+++ app/components/Resume.js
@@ -31,8 +31,15 @@ class Resume extends React.Component {
 
   render() {
     return (
-      <div className='container'>
-        <h1> Under Construction... Please click the LinkedIn Link for Information</h1>       
+      <div className='container fadeIn animated'>
+        <div className='col-xs-10 col-sm-10 col-md-8 col-md-offset-2' style={{paddingBottom: '200px;'}}>
+          <div className="panel panel-default text-center">
+            <br></br>
+            <text className='project-title'>Under Construction, Please Check out my  <a href="https://www.linkedin.com/in/jayteesanchez">Linked-In!</a></text>
+            <br></br>
+            <br></br>
+          </div>      
+        </div>      
       </div>
     )
   }
