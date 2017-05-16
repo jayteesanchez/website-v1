dd893418f41817f781835aaf660d724c2aadd685
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2017-05-15 22:30:19 -0700

    set to under construction

--- app/components/App.js
+++ app/components/App.js
@@ -6,9 +6,25 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <RouteHandler />
-        <footer></footer>
+        <nav className="navbar fadeIn animated">
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-2 xmargbot">
+              <span className='navbar-header shake animated'>
+                  <img id='name-brand' className=' col-xs-12 shake animated-8' src='/img/nameBrand.png'/>
+              </span>
+            </div>
+          </div>
+        </nav>
+        <div className='container fadeIn animated'>
+          <div className='col-xs-10 col-sm-10 col-md-8 col-md-offset-2' style={{paddingBottom: '200px;'}}>
+            <div className="panel panel-default text-center">
+              <br></br>
+              <text className='project-title'>Under Construction...Please Check out my  <a href="https://www.linkedin.com/in/jayteesanchez">Linked-In!</a></text>
+              <br></br>
+              <br></br>
+            </div>      
+          </div>      
+        </div>
       </div>
     );
   }
